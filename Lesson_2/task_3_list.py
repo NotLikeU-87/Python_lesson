5c47079139d24ff9caa88195f2733403b7768f45
NotLikeU-87 <notlikeu87@gmail.com>
2022-04-11 09:06:37 +0300

    add tasks

---
+++ Lesson_2/task_3_list.py
@@ -0,0 +1,18 @@
+"""
+3. Пользователь вводит месяц в виде целого числа от 1 до 12.
+Сообщить, к какому времени года относится месяц (зима, весна, лето, осень).
+Напишите решения через list и dict.
+"""
+season_list = ['Зима', 'Весна', 'Лето', 'Осень']
+season_dict = {1: 'Зима', 2: 'Весна', 3: 'Лето', 4: 'Осень'}
+month = int(input('Ведите номер месяца: '))
+if month == 12 or month == 1 or month == 2:
+    print('Время года: ', season_list[0])
+elif month == 3 or month == 4 or month == 5:
+    print('Время года: ', season_list[1])
+elif month == 6 or month == 7 or month == 8:
+    print('Время года: ', season_list[2])
+elif month == 9 or month == 10 or month == 11:
+    print('Время года: ', season_list[3])
+else:
+    print('такого месяца нет')
